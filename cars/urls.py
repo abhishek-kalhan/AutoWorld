d729d077d91bc1ce58a11031c8ea6a800fca1754
Abhishek Kalhan <abhishekkalhan@Abhisheks-MacBook-Air.local>
2023-07-23 16:03:56 -0400

    Car Detail and Search

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('cars', views.cars, name='cars'),
-    
+    path('cars/<int:id>', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search')
 ]
